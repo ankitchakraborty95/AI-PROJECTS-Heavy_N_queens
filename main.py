18ae6fc0c549b3c7a5725672f71a2043f20b14f4
ankitchakraborty95 <44779111+ankitchakraborty95@users.noreply.github.com>
2020-02-23 21:31:04 -0500

    latest_main func - needs A*

--- main.py
+++ main.py
@@ -1,36 +1,54 @@
-from random_board_generator_function import generate_queenboard
-from random_board_generator_function import print_board
 from common_functions import *
-import csv
+from hc_with_restart import hc_restart
 import sys
 
 # Generates a board with a random size
 def rand_n_queens(alg, heuristic):
+    print("alg selected",alg)
+    print("heuristic selected",heuristic)
     random.seed(random.random())
-    board = (generate_queenboard(random.randrange(5, 100)))
+    board = (generate_queenboard(random.randrange(5,15)))
+    print("start state")
     print_board(board)
     k = find_no_of_attacking_queens(board)
     h = calc_h(int(heuristic), k)
-    print(h)
+    print("attcking pair in input board", k)
+    print("input board Heuristic",h)
+    if alg == 'HC':
+         hc_restart(board,int(heuristic))
     pass
 
 # Generates a board with a specified size
 def num_n_queens(num, alg, heuristic):
+    print("number selected",num)
+    print("alg selected", alg)
+    print("heuristic selected",heuristic)
     board = (generate_queenboard(num))
+    print("start state")
     print_board(board)
     k = find_no_of_attacking_queens(board)
     h = calc_h(int(heuristic), k)
-    print(h)
+    print("attcking pair in input board",k)
+    print("input board Heuristic", h)
+    if alg == 'HC':
+        print("doing HC")
+        hc_restart(board,int(heuristic))
     pass
 
 # Uses a given csv file
 def n_queens(fname, alg, heuristic):
+    print("alg selected", alg)
+    print("heuristic selected", heuristic)
     board = read_board(fname)
-    for x in board:
-        print(x)
+    print("start state")
+    print_board(board)
     k = find_no_of_attacking_queens(board)
-    h = calc_h(int(heuristic), k)
-    print(k)
+    h = calc_h(int(heuristic),k)
+    print("attcking pair in input board",k)
+    print("input board Heuristic", h)
+    if alg == 'HC':
+        hc_restart(board,int(heuristic))
+
 
 def is_intstring (s):
     try:
@@ -44,8 +62,7 @@ if __name__ == "__main__":
         rand_n_queens(sys.argv[1], sys.argv[2])
     elif len(sys.argv) == 4 and is_intstring(sys.argv[1]):
         num_n_queens(int(sys.argv[1]), sys.argv[2], sys.argv[3])
-    elif len(sys.argv) == 4 and ~is_intstring(sys.argv[1]):
+    elif len(sys.argv) == 4 and is_intstring(sys.argv[1])!= True:
         n_queens(sys.argv[1], sys.argv[2], sys.argv[3])
     else:
         print("Ya got nothing")
-
